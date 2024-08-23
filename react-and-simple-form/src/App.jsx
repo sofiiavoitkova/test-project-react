6602f352c9bdc5381048b72a56af28f934da5290
Sofiia <sofiiavoitkova@gmail.com>
2024-08-23 21:20:15 +0200

    feat: add user registration form with confirmation dialog

--- react-and-simple-form/src/App.jsx
+++ react-and-simple-form/src/App.jsx
@@ -1,35 +1,121 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [user, setUser] = useState({});
+  const [confirmationOpen, setConfirmationOpen] = useState(false);
+  const [isConfirmed, setIsConfirmed] = useState(false);
+
+  const confirmUserData = (data) => {
+    setUser(data);
+    setConfirmationOpen(true);
+  };
+
+  const closeDialog = () => setConfirmationOpen(false);
+
+  const confirm = () => {
+    closeDialog();
+    setIsConfirmed(true);
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <main>
+        {isConfirmed ? (
+          `Congratulations user ${user.email}`
+        ) : (
+          <RegisterForm onSubmit={confirmUserData} />
+        )}
+      </main>
+      <ConfirmDialog
+        title="Please confirm registration"
+        cancel={closeDialog}
+        open={confirmationOpen}
+        confirm={confirm}
+      >
+        <p>Please confirm your email: {user.email}</p>
+      </ConfirmDialog>
     </>
-  )
+  );
 }
 
-export default App
+function RegisterForm({ onSubmit }) {
+  const [user, setUserData] = useState({ email: "", password: "" });
+
+  const setUserEmail = (e) => {
+    const email = e.target.value;
+    setUserData({ ...user, email });
+  };
+
+  const setUserPassword = (e) => {
+    const password = e.target.value;
+    setUserData({ ...user, password });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+
+    const { email, password } = user;
+
+    if (!email.includes("@")) return;
+
+    if (!password.trim()) return;
+
+    onSubmit(user);
+  };
+
+  return (
+    <>
+      <h1>Please register</h1>
+
+      <form onSubmit={handleSubmit}>
+        <AppInput
+          name="email"
+          label="Email"
+          type="email"
+          onChange={setUserEmail}
+          required={user.email}
+        />
+        <AppInput
+          name="pwd"
+          label="Password"
+          type="password"
+          onChange={setUserPassword}
+          required={user.password}
+        />
+        <button type="submit">Submit</button>
+      </form>
+    </>
+  );
+}
+
+function ConfirmDialog({ title, children, confirm, cancel, open }) {
+  return (
+    <dialog open={open}>
+      <div>{title}</div>
+      <div>{children}</div>
+      <div>
+        <button type="button" onClick={confirm}>
+          Confirm
+        </button>
+        <button type="button" onClick={cancel}>
+          Cancel
+        </button>
+      </div>
+    </dialog>
+  );
+}
+
+function AppInput({ label, name, ...rest }) {
+  return (
+    <div>
+      <label htmlFor={name}>{label}</label>
+      <input id={name} {...rest} />
+    </div>
+  );
+}
+
+export default App;
