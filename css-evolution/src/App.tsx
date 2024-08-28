2bf3ac30856e74df775c29a29bbf5b28ceb59a57
Sofiia <sofiiavoitkova@gmail.com>
2024-08-28 23:11:07 +0200

    feat: create basic form with React and SCSS styling

--- css-evolution/src/App.tsx
+++ css-evolution/src/App.tsx
@@ -1,35 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.scss';
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <main className="app">
+      <div className="root">
+        <label htmlFor="name">Name please</label>
+        <br />
+        <input className="input" id="name" type="text" />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <div className="root">
+        <label htmlFor="proficiency">Most experienced with</label>
+        <br />
+        <select name="proficiency" className="select">
+          <option value="good">React</option>
+          <option value="great">Angular</option>
+          <option value="super">Vue</option>
+        </select>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+      <div className="root">
+        <label htmlFor="experience">Experience</label>
+        <br />
+        <input className="input" type="number" id="experience" />
+      </div>
+    </main>
+  );
+};
 
-export default App
+export default App;
